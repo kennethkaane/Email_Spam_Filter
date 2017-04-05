37ab4d6ca3c20c340e5d0414932f488ccbb92821
StevenAvery <Steven.Avery@UOIT.net>
2017-04-05 12:54:29 -0400

    refactoring trainBayes.py

--- trainBayes.py
+++ trainBayes.py
@@ -23,103 +23,118 @@ SPAM_LABEL_FILE = DATA_DIR + "SPAMTrain.label"
 RESULT_FILE     = BUILD_DIR + "spam_filter_results" + str(TRAINING_SIZE)
 
 # load the spam labels into array (col 0 = label, col 2 = emailName)
-print "Loading spam labels"
+def loadSpamLabel():
+	print "Loading spam labels"
 
-spamLabel = np.loadtxt(SPAM_LABEL_FILE, dtype = {
-	"names": ("label", "emailName"),
-	"formats": ("i4", "S15",)})
+	spamLabel = np.loadtxt(SPAM_LABEL_FILE, dtype = {
+		"names": ("label", "emailName"),
+		"formats": ("i4", "S15",)})
 
-# limit the size of our training set
-spamLabel = np.take(spamLabel, range(TRAINING_SIZE))
+	# limit the size of our training set
+	spamLabel = np.take(spamLabel, range(TRAINING_SIZE))
 
-# P(spam) = total number of spam labels
-pSpam = np.sum([1 for i in spamLabel if i["label"] == SPAM_LABEL])
-# P(ham) = total number of ham labels
-pHam = np.sum([1 for i in spamLabel if i["label"] == HAM_LABEL])
+	return spamLabel
 
+# calculat counts for pSpam, and pHam
+def calculateLabelCounts(spamLabel):
+	# P(spam) = total number of spam labels
+	pSpam = np.sum([1 for i in spamLabel if i["label"] == SPAM_LABEL])
+	# P(ham) = total number of ham labels
+	pHam = np.sum([1 for i in spamLabel if i["label"] == HAM_LABEL])
+
+	return (pSpam, pHam)
 
 # build a vocabulary of available words
-print "Building vocabulary"
+def buildVocabulary(spamLabel):
+	print "Building vocabulary"
 
-vocabulary = np.array([])
+	vocabulary = np.array([])
 
-# go through the first TRAINING_SIZE emails in the dataset
-for email in spamLabel:
-	# open the current email
-	oFile = open(TRAIN_DIR + email["emailName"])
+	# go through the first TRAINING_SIZE emails in the dataset
+	for email in spamLabel:
+		# open the current email
+		oFile = open(TRAIN_DIR + email["emailName"])
 
-	# a list of words in the current email
-	for word in oFile.read().split():
-		# contains fails if the np.array is empty
-		if len(vocabulary) == 0:
-			vocabulary = np.append(vocabulary, word)
-		# if the word isn't already in the vocabulary, add it
-		elif not vocabulary.__contains__(word):
-			vocabulary = np.append(vocabulary, word)
+		# a list of words in the current email
+		for word in oFile.read().split():
+			# contains fails if the np.array is empty
+			if len(vocabulary) == 0:
+				vocabulary = np.append(vocabulary, word)
+			# if the word isn't already in the vocabulary, add it
+			elif not vocabulary.__contains__(word):
+				vocabulary = np.append(vocabulary, word)
+
+	return vocabulary
 
 # determine the probability that a word is in spam, and ham
-# these arrays should be the same shape as the vocabulary, and start at zero
-spamWordCount = np.zeros(len(vocabulary))
-hamWordCount  = np.zeros(len(vocabulary))
+def getWordProbabilities(spamLabel, pSpam, pHam, vocabulary):
+	# these arrays should be the same shape as the vocabulary, and start at zero
+	spamWordCount = np.zeros(len(vocabulary))
+	hamWordCount  = np.zeros(len(vocabulary))
 
-# To find word probabilities we must count the number of occurances each word
-print "Finding spam, and ham word counts"
+	# To find word probabilities we must count the number of occurances each word
+	print "Finding spam, and ham word counts"
 
-for email in spamLabel:
-	# open the current email
-	oFile = open(TRAIN_DIR + email["emailName"])
+	for email in spamLabel:
+		# open the current email
+		oFile = open(TRAIN_DIR + email["emailName"])
 
-	# form a list of words found in this email (no duplicates)
-	emailVocabulary = np.array([])
+		# form a list of words found in this email (no duplicates)
+		emailVocabulary = np.array([])
 
-	# a list of words in the current email
-	for word in oFile.read().split():
-		# contains fails if the np.array is empty
-		if len(emailVocabulary) == 0:
-			emailVocabulary = np.append(emailVocabulary, word)
-		# if the word isn't already in the vocabulary, add it
-		elif not emailVocabulary.__contains__(word):
-			emailVocabulary = np.append(emailVocabulary, word)
+		# a list of words in the current email
+		for word in oFile.read().split():
+			# contains fails if the np.array is empty
+			if len(emailVocabulary) == 0:
+				emailVocabulary = np.append(emailVocabulary, word)
+			# if the word isn't already in the vocabulary, add it
+			elif not emailVocabulary.__contains__(word):
+				emailVocabulary = np.append(emailVocabulary, word)
 
-	# if the email is spam, add the words to spamWordCount
-	if email["label"] == SPAM_LABEL:
-		for word in emailVocabulary:
-			spamWordCount[np.where(vocabulary==word)] += 1
-	# if the email is ham, add the words to hamWordCount
-	elif email["label"] == HAM_LABEL:
-		for word in emailVocabulary:
-			hamWordCount[np.where(vocabulary==word)] += 1
+		# if the email is spam, add the words to spamWordCount
+		if email["label"] == SPAM_LABEL:
+			for word in emailVocabulary:
+				spamWordCount[np.where(vocabulary==word)] += 1
+		# if the email is ham, add the words to hamWordCount
+		elif email["label"] == HAM_LABEL:
+			for word in emailVocabulary:
+				hamWordCount[np.where(vocabulary==word)] += 1
 
-# probability a word is in spam, and ham
-print "Determining spam and ham word probabilities"
+	# probability a word is in spam, and ham
+	print "Determining spam and ham word probabilities"
 
-pSpamWord = [wordCount/pSpam for wordCount in spamWordCount]
-pHamWord  = [wordCount/pHam for wordCount in hamWordCount]
+	pSpamWord = [wordCount/pSpam for wordCount in spamWordCount]
+	pHamWord  = [wordCount/pHam for wordCount in hamWordCount]
+
+	return (pSpamWord, pHamWord)
 
 
-# DEBUGING
-print "\nRunning tests"
-# check that no probability is greater than one
-pSpamWordCheck = np.sum([1 for i in pSpamWord if i > 1.0])
-if pSpamWordCheck == 0.0:
-	print "\tPASSED: pSpamWord Check"
-else:
-	print "\tFAILED: pSpamWord Check with value: ", pSpamWordCheck
+# run tests to make sure our results make sence
+def verifyResults(pSpamWord, pHamWord):
+	print "\nRunning tests"
+	# check that no probability is greater than one
+	pSpamWordCheck = np.sum([1 for i in pSpamWord if i > 1.0])
+	if pSpamWordCheck == 0.0:
+		print "\tPASSED: pSpamWord Check"
+	else:
+		print "\tFAILED: pSpamWord Check with value: ", pSpamWordCheck
 
-pHamWordCheck = np.sum([1 for i in pHamWord if i > 1.0])
-if pHamWordCheck == 0.0:
-	print "\tPASSED: pHamWord Check"
-else:
-	print "\tFAILED: pHammWord Check with value: ", pHamWordCheck
+	pHamWordCheck = np.sum([1 for i in pHamWord if i > 1.0])
+	if pHamWordCheck == 0.0:
+		print "\tPASSED: pHamWord Check"
+	else:
+		print "\tFAILED: pHammWord Check with value: ", pHamWordCheck
+
 
 # save the results to an output file
-np.savez(RESULT_FILE, pSpam, pHam, pSpamWord, pHamWord)
+def saveResults(pSpam, pHam, pSpamWord, pHamWord):
+	np.savez(RESULT_FILE, pSpam, pHam, pSpamWord, pHamWord)
 
 
-# DEBUGING
-#print "spamLabel:", spamLabel
-#print "pSpam:", pSpam
-#print "pHam:", pHam
-#print "vocabulary:", vocabulary
-#print "pSpamWord:", pSpamWord
-#print "pHamWord:", pHamWord
+spamLabel = loadSpamLabel()
+pSpam, pHam = calculateLabelCounts(spamLabel)
+vocabulary = buildVocabulary(spamLabel)
+pSpamWord, pHamWord = getWordProbabilities(spamLabel, pSpam, pHam, vocabulary)
+
+verifyResults(pSpamWord, pHamWord)
+saveResults(pSpam, pHam, pSpamWord, pHamWord)
